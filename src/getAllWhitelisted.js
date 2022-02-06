34dbc92262506609acb0e708aceddd97b529b4d2
Francesco Sullo <sullof@gmail.com>
2022-02-06 09:15:10 -0800

    Preparing lottery

--- src/getAllWhitelisted.js
+++ src/getAllWhitelisted.js
@@ -2,62 +2,27 @@ const fspath = require('fspath')
 
 const argWinners = require('../data/arg-winners.json')
 const goldmineWinners = require('../data/goldmine-winners.json')
-const goldmineWinners = require('../data/giv')
-const rarity = require('../data/rarity.json')
+const giveawayWinners = require('../data/giveaway-winners.json')
+const team = require('../data/team-wallets.json')
+const lotteryWinners = require('../data/lottery-winners.json')
+
+const whitelist = {}
+const allWinners = []
+    .concat(argWinners)
+    .concat(goldmineWinners)
+    .concat(giveawayWinners)
+    .concat(lotteryWinners)
+    .concat(team)
+
+// console.log(allWinners.length)
+
+(
+    async function main() {
 
 
-
-let res = []
-
-let firstRow = {Name: 1}
-
-async function main() {
-
-  for (let m of argWinners) {
-    if (~selected.indexOf(m.name)) {
-      let row = {}
-      let i = res.length
-      res[i] = [m.name, row]
-
-      for (let a of m.attributes) {
-        if (!firstRow[a.trait_type]) {
-          firstRow[a.trait_type] = 1
-        }
-        row[a.trait_type] = [a.value, rarity[a.trait_type][a.value] + '%']
-      }
     }
-  }
-
-  let traits = Object.keys(firstRow).slice(1)
-  res[0] = ['Name']
-  for (let t of traits) {
-    res[0].push(t, 'Rarity')
-  }
-
-  for (let i = 1; i< res.length;i++) {
-    let data = res[i][1]
-    res[i][1] = ''
-    for (let d in data) {
-      let index = res[0].indexOf(d)
-      let values = data[d]
-      res[index] =
-    }
-  }
-
-  //
-  // res.sort((a,b) => {
-  //   a = a[0]
-  //   b = b[0]
-  //   return a >b ? 1 : a < b ? -1 : 0
-  // })
-  //
-  // res[0][0] = 'Name'
-  //
-  // stringify(res, function(err, output){
-  //   const csv = new fspath('data/selectedProperties.csv')
-  //   csv.write(output)
-  // })
-
-}
-
-main()
+)().then(() => process.exit(0))
+    .catch(error => {
+      console.error(error);
+      process.exit(1)
+    })
