e1a4714bef48b31f08480f6cdb66eaebbf0967d7
Francesco Sullo <sullof@gmail.com>
2022-02-06 18:17:50 -0800

    Minor changes

--- src/getAllWhitelisted.js
+++ src/getAllWhitelisted.js
@@ -1,27 +1,56 @@
 const fspath = require('fspath')
-
+const keccak256 = require('keccak256')
+const ethers = require('ethers')
 const argWinners = require('../data/arg-winners.json')
 const goldmineWinners = require('../data/goldmine-winners.json')
 const giveawayWinners = require('../data/giveaway-winners.json')
 const team = require('../data/team-wallets.json')
-const lotteryWinners = require('../data/lottery-winners.json')
+const lotteryWinners = require('../data/28-lottery-winners.json')
 
-const whitelist = {}
-const allWinners = []
-    .concat(argWinners)
-    .concat(goldmineWinners)
-    .concat(giveawayWinners)
-    .concat(lotteryWinners)
-    .concat(team)
+console.log('giveawayWinners', giveawayWinners.length)
+console.log('goldmineWinners', goldmineWinners.length)
+console.log('argWinners', argWinners.length)
+console.log('team', team.length)
+console.log('lotteryWinners', lotteryWinners.length)
 
-// console.log(allWinners.length)
+async function main() {
 
-(
-    async function main() {
+  const whitelist = []
+  const tmp = {}
+  const allWinners = []
+      .concat(team)
+      .concat(giveawayWinners)
+      .concat(goldmineWinners)
+      .concat(argWinners)
+      .concat(lotteryWinners)
 
+  console.log('Total', allWinners.length)
 
+  for (let winner of allWinners) {
+    if (!winner.removed) {
+      if (!winner.wallet) {
+        console.log('Wallet missing for', winner)
+        winner.wallet = ethers.Wallet.createRandom().address
+      }
+      tmp[winner.wallet] = (tmp[winner.wallet] || 0) + 1
     }
-)().then(() => process.exit(0))
+  }
+  let nextTokenId = 1
+  for (let address in tmp) {
+    let tokenIds = []
+    for (let j=0;j<tmp[address];j++) {
+      tokenIds.push(nextTokenId++)
+    }
+    whitelist.push({
+      address,
+      tokenIds
+    })
+  }
+  let output = new fspath('data/whitelist.json')
+  output.write(JSON.stringify(whitelist, null, 2))
+}
+
+main().then(() => process.exit(0))
     .catch(error => {
       console.error(error);
       process.exit(1)
